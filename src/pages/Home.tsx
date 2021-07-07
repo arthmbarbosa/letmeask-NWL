93f2506bbaf0e81c9ed30b728f1a9ff84d3e8da0
Arthur Barbosa <arthur_barbosa97@hotmail.com>
2021-07-07 00:56:46 -0300

    create Navgation | Google login
    Creation of navigation links between pages and login authentication with Google(Not done yet).

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from '../App';
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -6,7 +10,20 @@ import '../styles/auth.scss';
 import '../styles/button.scss';
 import { Button } from '../components/Button';
 
+
+
 export function Home() {
+    const history = useHistory();
+    const { user, signInWithGoogle } = useContext(AuthContext)
+
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+
+        history.push('/rooms/new');
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -17,7 +34,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -5,8 +7,12 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss';
 import '../styles/button.scss';
 import { Button } from '../components/Button';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 export function NewRoom() {
+    const { user } = useContext(AuthContext)
+
     return (
         <div id="page-auth">
             <aside>
@@ -17,6 +23,7 @@ export function NewRoom() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
+                    <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
                     <form>
                         <input
@@ -27,7 +34,7 @@ export function NewRoom() {
                             Criar sala
                         </Button>
                     </form>
-                    <p>Quer entrar em uma sala existente? <a href="#" >Clique aqui</a></p>
+                    <p>Quer entrar em uma sala existente? <Link to="/" >Clique aqui</Link></p>
                 </div>
             </main>
         </div>
