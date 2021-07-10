6455927425bea33265adc8290067ecc74d1ef26c
Arthur Barbosa <arthur_barbosa97@hotmail.com>
2021-07-10 12:19:48 -0300

    create IsAnswered and IsHighlighted function in Questions

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-
+import cx from 'classnames'
 import './style.scss';
 
 type QuestionProps = {
@@ -9,13 +9,26 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 }
 
 export function Question({
-  content, author, children
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children
+
+
 }: QuestionProps) {
   return (
-    <div className="question">
+    <div className={cx(
+      'question',
+      { answered: isAnswered },
+      { highlighted: isHighlighted },
+    )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
